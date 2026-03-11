82baae7cb12f40e0ed37b322b54b43f8dc4e90b7
codewithnanu <gauranisha3@gmail.com>
2026-03-11 14:49:35 +0530

    fixed index.js

--- todo-main/server/index.js
+++ todo-main/server/index.js
@@ -91,7 +91,4 @@ app.get("/", (req, res) => {
   res.json({ status: "Backend running 🚀" });
 });
 
-// ---------------- START SERVER ----------------
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
